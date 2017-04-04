ef00f228e8b25dbb1e64710d04e6f47dbe33b80d
Noriaki Oshita <ponkichi0213@gmail.com>
2017-04-04 15:50:16 +0900

    [add]重複しない名詞だけ抽出するseparate.pyを追加.

---
+++ tango/separate.py
@@ -0,0 +1,21 @@
+import sys
+import MeCab
+import os
+import codecs
+
+m = MeCab.Tagger(' -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+
+text = sys.argv[1]
+tagger = MeCab.Tagger()
+keywords = []
+with codecs.open(os.path.join('./', text),'r', 'utf-8') as f:
+    context = f.read()
+    #encoded_text = context.encode('utf-8')
+    node = tagger.parseToNode(context).next
+    while node:
+        # 名詞,一般の行だけを抽出する．
+        if node.feature.split(",")[0] == "名詞" and node.feature.split(",")[1] == "一般":
+            if node.surface not in keywords:
+                keywords.append(node.surface)
+        node = node.next
+    print(' '.join(keywords))
