533ddc60e02c320baa84dc7f039bd8fed81569b5
Noriaki Oshita <ponkichi0213@gmail.com>
2017-04-14 22:02:23 +0900

    [update]README.mdの変更と，モジュールの変更.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# Mojikara
-文字から画像を返すAPI
+Mojikarai  
+====
+Pythonで書かれた，文章から画像を返すプログラム  
+  
+##説明  
+このプログラム群はPythonで書かれた文章から特徴量を抽出して，そのワードに関連する画像のリンクを検索するものです．  
+画像検索にはgoogleのCustom Search APIを使用しています．
+そのapiの詳細は[公式ドキュメント](https://developers.google.com/custom-search/docs/overview?hl=ja)をご覧ください.  
+
+実際の特徴量選択については現在考えておりますが，TF IDFにて重み付けされた単語を使用してみましたが，あまりいい精度が出ませんでした．現在は単純に頻出度の最大値で画像検索をしております．近いうちにもっと良い方法に変更するかもしれません．  
+  
+
+##使用方法
+※このプログラムでは説明文にあるように，Custom Search APIを使用していますので，クライアントIDとエンジンIDを取得する必要があります．　　
+  
+環境変数PYTHONPATHにmoduleのパスを追加してください.  
+
+[例]  
+
+```
+  export PYTHONPATH = '/Users/Ooshita/Mojikara/src/module/'
+```
+
+あとはimportとしてご使用ください．  
+  
+実際のサンプルはsrcディレクトリ以下のtestディレクトリのtest.pyを参考にしてください．
+
+
+##ライセンス  
+[![License](https://img.shields.io/badge/license-Apache%202-blue.svg)]  
+(https://www.apache.org/licenses/LICENSE-2.0)  
+
+##著作者  
+[Ooshita](https://github.com/Ooshita)
+
--- src/module/get_feature_value.py
+++ src/module/get_feature_value.py
@@ -1,46 +1,14 @@
-import sys
-import os
-import codecs
-import numpy as np
 import MeCab
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction import DictVectorizer
-from collections import Counter
-
-class Get_feature(self):
+keywords = []
+m = MeCab.Tagger(' -d /usr/local/lib/mecab/dic/mecab-ipadic-neologd')
+    
+class Get_feature:
     # 起動時に一度だけ動く処理
     def __init__(self):
         self.document = ""
-        count = CountVectorizer():
-        keywords = []
-
-        
-        
-    def result(threshold):
-        hairetsu = np.where(tfidf_array >= threshold)
-        hit_word = np.unique(np.append(hairetsu[0], hairetsu[1]))
-        word = []
-        # tfidfで計算された単語を出力する．
-        for num in range(0, len(hit_word)):
-            word.append(feature[hit_word[num]])
-        return word
-
-    def morphResult():
-        
-        # tfidfの設定
-        tfidf = TfidfTransformer(use_idf=True, norm='l2', smooth_idf=True)
-        # 小数点第4位まで表示する．
-        np.set_printoptions(precision=4)
-        cnt_voc = count.vocabulary_
-        tfidf_array = np.array(tfidf.fit_transform(bag).toarray())
-
-        # ベクトルと単語を対応づけるために必要な処理．
-        vec = DictVectorizer()
-        vec.fit_transform(cnt_voc).toarray()
-        feature = vec.get_feature_names()
-
-        for word in docs:
+    
+    def morph_result(document):
+        for word in document:
             node = m.parseToNode(word).next
             while node:
                 nodeFeature = node.feature.split(",")
@@ -49,21 +17,3 @@ class Get_feature(self):
                     keywords.append(node.surface)
                 node = node.next
         return keywords
-
-    word_dict = {}
-    # 単語の頻出度を調べる
-    countWord = Counter(morphResult())
-    for word, cnt in countWord.most_common():
-        word_dict.update({word:cnt})
-        print(word, cnt)
-
-    # 辞書の最大値を表示
-    # print(max(word_dict.values()))
-
-
-    # 最大値のキーを表示
-    for word, value in word_dict.items():
-        if value == max(word_dict.values()):
-            print("最頻出単語: " + word)   
-        
-
