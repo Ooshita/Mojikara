34e040ade3828261041895ce6ab155ec6374f8bd
Noriaki Oshita <ponkichi0213@gmail.com>
2017-06-08 22:55:56 +0900

    [fix]while文が2重ループになってて１文字ずつ解析してて変な結果しか返ってきてなかった．

--- src/module/get_feature_value.py
+++ src/module/get_feature_value.py
@@ -9,14 +9,13 @@ class Get_feature:
         self.document = ""
     
     def morph_result(document):
-        for word in document:
-            node = m.parseToNode(word).next
-            while node:
-                nodeFeature = node.feature.split(",")
-                # 名詞,一般と名詞．名詞,固有名のみ抽出
-                if nodeFeature[0] == "名詞" and nodeFeature[1] == "一般" or  nodeFeature[0] == "名詞" and nodeFeature[1] == "固有名詞":
-                    keywords.append(node.surface)
-                node = node.next
+        node = m.parseToNode(document).next
+        while node:
+            nodeFeature = node.feature.split(",")
+            # 名詞,一般と名詞．名詞,固有名のみ抽出
+            if nodeFeature[0] == "名詞" and nodeFeature[1] == "一般" or  nodeFeature[0] == "名詞" and nodeFeature[1] == "固有名詞":
+                keywords.append(node.surface)
+            node = node.next
         return keywords
 
     def most_word(keywords):
